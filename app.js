cc51ed5ef1ef80c359e144eff5e784829109c076
Chantae P <60330899+taepal467@users.noreply.github.com>
2022-07-13 10:45:59 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,67 +1,77 @@
 
-const buttons = document.getElementsByClassName('btn');
-
-const playerWeapon = () => {
-  for (let i = 0 ; i < buttons.length; i++) {
-    buttons[i].addEventListener('click' , () => {
-      let playerSelection = buttons[i].textContent;
-      document.querySelector(".player-weapon").textContent = playerSelection;
-      console.log("playerOption: ",playerSelection);    
-    }); 
-  }
-}
-
 const computerWeapon = () => {
   let gameChoices = ["🪨", "📜", "✂"];
   let random = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
   let computerWeapon = document.querySelector(".computer-weapon");
   computerWeapon.textContent = random;
-  console.log("computerOption: ",random);
 
   return random;
 }
 
-const selectedWeapons = () => {
+const playRound = () => {
+  const buttons = document.getElementsByClassName('btn');
+
   for  (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", computerWeapon);
   }
+
+  for (let i = 0 ; i < buttons.length; i++) {
+    buttons[i].addEventListener('click' , () => {
+      let playerSelection = buttons[i].textContent;
+      document.querySelector(".player-weapon").textContent = playerSelection;
+      
+      computerSelection = computerWeapon();
+
+      console.log("playerOption2: ", playerSelection);
+      console.log("computerOption2: ", computerSelection);
+    
+      if (computerSelection === "📜" && playerSelection === "🪨") {
+        console.log("Computer Wins!");
+      } else if (playerSelection === "📜" && computerSelection === "🪨") {
+        console.log("You Win!");
+      }
+    
+      if (computerSelection === "✂" && playerSelection === "📜") {
+        console.log("Computer Wins!");
+      } else if (playerSelection === "✂" && computerSelection === "📜") {
+        console.log("You Win!");
+      }
+    
+      if (computerSelection === "🪨" && playerSelection === "✂") {
+        console.log("Computer Wins!");
+      } else if (playerSelection === "🪨" && computerSelection === "✂") {
+        console.log("You Win!");
+      }
+    
+      if (computerSelection === playerSelection) {
+          console.log("Tie!");
+      }
+
+      if (playerWin) {
+        playerScoreCount ++;
+        console.log("player score: ", playerScoreCount);
+      } 
+
+      if (computerWin) {
+        computerScoreCount ++;
+        console.log("computer score: ", computerScoreCount);
+      }
+
+      // if (playerScoreCount === 5 || computerScoreCount === 5) {
+      //   break;
+      // }
+    }); 
+  }
 }
 
-const playRound = (playerSelection, computerSelection) => {
-  
-  playerSelection = playerWeapon();
-  computerSelection = selectedWeapons();
+let playerWin = "You Win!";
+let computerWin = "Computer Wins!";
+let gameTie = "Tie!";
+let playerScoreCount = 0;
+let computerScoreCount = 0;
 
-  console.log("playerWeapon: ", playerSelection);
-  console.log("computerWeapon: ", computerSelection);
-
-  if (computerSelection === "📜" && playerSelection === "🪨") {
-    console.log("Computer Wins!");
-  } else if (playerSelection === "📜" && computerSelection === "🪨") {
-    console.log("You Win!");
-  }
-
-  if (computerSelection === "✂" && playerSelection === "📜") {
-    console.log("Computer Wins!");
-  } else if (playerSelection === "✂" && computerSelection === "📜") {
-    console.log("You Win!");
-  }
-
-  if (computerSelection === "🪨" && playerSelection === "✂") {
-    console.log("Computer Wins!");
-  } else if (playerSelection === "🪨" && computerSelection === "✂") {
-    console.log("You Win!");
-  }
-
-  if (computerSelection === playerSelection) {
-      console.log("Tie!");
-  }
- 
-}
-
-
-console.log("playRound: ", playRound());
+playRound();
 
 
 
